5ed77cab2e2583e79f4f5e4e9d785f72987d8dd2
Quanwei <573011406@qq.com>
2016-11-24 19:08:00 +0800

    first commit

---
+++ BeijingJinJingZheng/JinjingZhengAPI/JinjingZhengAPI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JumpKick.HttpLib;
+using JumpKick.HttpLib.Provider;
+namespace JinjingZheng
+{
+    public class JinJingZhengAPI
+    {
+        public static void SendVerifyCode(string phone,string regist = "1")
+        {
+            var req = Http.Post("https://bjjj.zhongchebaolian.com/common_api/mobile/standard/verification");
+            //req.Headers(HeaderProvider.)
+            req.Body("{\"phone\":\"13621210918\",\"regist\":\"1\"}").OnSuccess((str) => {
+                System.Diagnostics.Debug.WriteLine(str);
+            }).OnFail((ex) => {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }).Go();
+        }
+
+
+    }
+}
